473c0f5b8d5fd0385fd1188253551b03d1761968
Kakashi1hatake <kushalghising586@gmail.com>
2020-11-09 13:37:10 +0545

    Add Files

---
+++ To Dolist/main.js
@@ -0,0 +1,8 @@
+let butn=document.getElementById('btn');
+let cont=document.getElementById('con');
+let text=document.getElementById('txt');
+
+butn.addEventListener('click',function(){
+	var paragraph=document.createElement(p)
+	cont.appendchild(paragraph);
+	})
