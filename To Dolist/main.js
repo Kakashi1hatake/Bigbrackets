0ab7a5a35be76bf1e825fd7d605d2fc94c1638ad
Kakashi1hatake <kushalghising586@gmail.com>
2020-11-10 09:16:48 +0545

    message

--- To Dolist/main.js
+++ To Dolist/main.js
@@ -1,13 +1,16 @@
-function todolist(){
-	var item = document.getElementById('txt').value;
-	var text =document.createTextNode(item);
-	var newItem=document.createElement("li");
-	newItem.appendChild(text);
-	document.getElementById("todol").appendChild(newItem);
-	txt.value="";
-
-
-	
-	
-	
+function onFormSubmit(e){
+	e.preventDefault()
+	var formData={}
+	formData['title']=document.getElementById('title').value
+	var table= document.getElementById('data')
+	var row=table.insertRow()
+	cell1=row.insertCell(0);
+	cell1.innerText=formData.title
+	cell1=row.insertCell(1);
+	cell1.innerHTML=`<button onclick="onDelete(this)">Delete</button>`
+}
+function onDelete(td)
+{
+	var rowToBeDeleted=td.parentElement.parentElement
+	document.getElementById('data').deleteRow(rowToBeDeleted.rowIndex)
 }
