0ece4e0e8b0f71b563e4d0e2f8ada18a475723f2
Kakashi1hatake <kushalghising586@gmail.com>
2020-11-11 09:27:51 +0545

    Edit

---
+++ Domain/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', ()=>{
+    const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const email = params.get('email');
+    const mobile = params.get('phone');
+    const domainName = params.get('domain');
+    const domainExtension = params.get('domainEx');
+
+    document.querySelectorAll('.name').forEach(item=>{
+        item.innerText = name
+    })
+    document.querySelector('#email').innerText = email
+    document.querySelector('#mobile').innerText = mobile
+    document.querySelectorAll('.domain-extension').forEach(item=>{
+        item.innerText = domain+"."+domainEx
+    })
+    document.querySelector('#just-domain-extension').innerText = domainEx
+})
